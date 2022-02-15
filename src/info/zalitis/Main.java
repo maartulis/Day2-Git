3b251a7a77a14a203ccc94f4dccd358a750479fe
martins.zalitis <martins.zalitis@gmail.com>
2022-02-15 11:18:01 +0200

    Task 1 declard, initilised primitive variables (half way done)

--- src/info/zalitis/Main.java
+++ src/info/zalitis/Main.java
@@ -6,5 +6,13 @@ public class Main {
         //Please write a Java program which declares,
         // assign values of each primitive data type
         // and print out in console the result
+        byte byteNumber = 1;
+        short shortNumber = 2;
+        int intNumber = 3;
+        long longNumber = 4;
+        float floatNumber = 5.0f;
+        double doubleNumber = 6.0;
+        char charChar = 'c';
+        boolean booleanValue = false;
     }
 }
