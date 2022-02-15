ce9722bb32cfab01a810c5f96a2b6f45a889e734
martins.zalitis <martins.zalitis@gmail.com>
2022-02-15 11:07:24 +0200

    Initial commit of example task

---
+++ src/info/zalitis/Main.java
@@ -0,0 +1,8 @@
+package info.zalitis;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //we want to cast char to integer e.g. char letterA = A to int numversionOfLetterA;
+    }
+}
