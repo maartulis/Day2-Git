f1b3e9e5f64a82e7337e74c95836e37b16878e5e
martins.zalitis <martins.zalitis@gmail.com>
2022-02-15 11:12:55 +0200

    Complete casting task

--- src/info/zalitis/Main.java
+++ src/info/zalitis/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         //we want to cast char to integer e.g. char letterA = A to int numversionOfLetterA;
+        char  letterA = 'A';
+        int letterAInNumericVersion = Character.getNumericValue(letterA);
+        System.out.println("char letterA: " + letterA);
+        System.out.println("int letterAInNumericVersion " + letterAInNumericVersion);
+        letterAInNumericVersion = letterA;
+        System.out.println(letterAInNumericVersion);
     }
 }
